c3226056734509299305bb5f2a23d5728f5d240f
Guillaume Corbelin <guillaume.corbelin@coexya.eu>
2023-03-22 10:04:35 +0100

    Add empty tests todo for Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,6 +3,7 @@
  */
 
 import { screen, waitFor } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
@@ -11,6 +12,16 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills page but it is loading", () => {
+    test("Then, Loading page should be rendered", () => {
+      // Write test
+    });
+  });
+  describe("When I am on Bills page but back-end send an error message", () => {
+    test("Then, Error page should be rendered", () => {
+      // Write test
+    });
+  });
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
       Object.defineProperty(window, "localStorage", {
@@ -41,6 +52,35 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+  });
+  describe("When I am on Bills Page and I click on the 'New bill' button", async () => {
+    test("I should be sent on the New Bill page", async () => {
+      // Write test
+    });
+  });
+  describe("When I am on Bills Page and I click on the first eye icon", async () => {
+    test("A modal should open", async () => {
+      // Write test
+    });
+  });
+});
+
+// test d'intégration GET
+describe("Given I am a user connected as Admin", () => {
+  describe("When I navigate to Dashboard", () => {
+    test("fetches bills from mock API GET", async () => {
+      // Write test
+    });
+    describe("When an error occurs on API", () => {
+      // Add before each with call to API
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        // Write test
+      });
+
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        // Write test
+      });
     });
   });
 });
