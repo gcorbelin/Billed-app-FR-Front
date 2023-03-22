172f954e90dda088f365c5c57ecb92277f4f532c
Guillaume Corbelin <guillaume.corbelin@coexya.eu>
2023-03-22 14:48:28 +0100

    test highlight icon NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,16 +2,38 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom";
+import { screen, waitFor } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
+import router from "../app/Router.js";
+
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI();
-      document.body.innerHTML = html;
-      //to-do write assertion
+    test("Then envelope icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-window"));
+      const windowIcon = screen.getByTestId("icon-window");
+      expect(windowIcon).not.toHaveClass("active-icon");
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const mailIcon = screen.getByTestId("icon-mail");
+      expect(mailIcon).toHaveClass("active-icon");
     });
   });
 });
